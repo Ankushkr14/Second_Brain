1f144be2a8505453a1b62863b96b6e8d7c24c2e9
Ankushkr14 <pro.bittuankush14@gmail.com>
2025-07-17 11:44:09 +0530

    updated config

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -13,6 +13,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:3000',
+        'https://second-brain-xi-three.vercel.app/'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
